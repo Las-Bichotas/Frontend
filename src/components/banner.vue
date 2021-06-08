ec0544f08f83d1c2825e5379211e84e5b2801b59
XantX <hp_xantx@hotmail.com>
2021-06-07 22:31:48 -0500

    feat: add banner component and images to banner

---
+++ src/components/banner.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-banner elevation="4">
+    <v-card>
+      <v-card-title>Titulo inpirador</v-card-title>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn large elevation="2">Login</v-btn>
+        <v-btn large elevation="2">Register</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-banner>
+</template>
+
+<script>
+export default {
+  name: "banner",
+};
+</script>
+
+<style scoped></style>
