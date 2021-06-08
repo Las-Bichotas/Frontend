8951b74fb93f792f4e0ce19151ec020f9c294d25
XantX <hp_xantx@hotmail.com>
2021-06-08 02:35:30 -0500

    feat: add banner corousel tODO: impolemente login

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -1,19 +1,38 @@
 <template>
   <v-banner elevation="4">
-    <v-card>
-      <v-card-title>Titulo inpirador</v-card-title>
-      <v-divider></v-divider>
-      <v-card-actions>
-        <v-btn large elevation="2">Login</v-btn>
-        <v-btn large elevation="2">Register</v-btn>
-      </v-card-actions>
-    </v-card>
+    <div class="d-flex justify-space-around align-center">
+      <div class="mr-16 ml-16">
+        <div class="text-h1 mb-5">
+          ILanguage
+        </div>
+        <p class="text-h6">
+          La mejor forma de estudiar un idioma platicando con nativos del
+          lenguage que prefieras
+        </p>
+      </div>
+      <v-carousel hide-delimiters cycle :show-arrows="false" height="600">
+        <v-carousel-item
+          v-for="(item, i) in items"
+          :key="i"
+          :src="item.src"
+        ></v-carousel-item>
+      </v-carousel>
+    </div>
   </v-banner>
 </template>
 
 <script>
 export default {
   name: "banner",
+  data() {
+    return {
+      items: [
+        { src: require("../assets/zoom-conversation.jpg") },
+        { src: require("../assets/conversation-twomens.jpg") },
+        { src: require("../assets/conversation-twowomans.jpg") },
+      ],
+    };
+  },
 };
 </script>
 
