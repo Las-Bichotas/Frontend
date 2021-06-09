6aa264166b1581712efceb53bc00ecb684deb7d8
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2021-06-09 13:01:39 -0500

    feat: add card about languages more spoken TODO: add styles

---
+++ src/components/more-spoken.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container>
+    <v-row gutters>
+      <v-col cols="6" md="6" sm="12">
+        <v-card elevation="20">
+          <v-card-title>Idiomas más hablados</v-card-title>
+          <v-card-subtitle>Inglés</v-card-subtitle>
+          <v-card-text>El idioma inglés es un idioma indoeuropeo del grupo lingüístico germánico occidental. El inglés moderno
+            es ampliamente considerado como la lengua franca del mundo y es el idioma estándar en un amplia variedad de campos,
+            incluida la codificación informática, los negocios internacionales y la educación superior..</v-card-text>
+          <v-img height="200px" src="https://images.pexels.com/photos/968299/pexels-photo-968299.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"> </v-img>
+          <v-card-subtitle>Chino Mandarín</v-card-subtitle>
+          <v-card-text>Aprender mandarín te da la oportunidad de ver más de cerca al mercado chino, considerado el segundo más importante a nivel mundial.
+            Comerciar con China es visto como una de las mejores decisiones estratégicas en el mundo de los negocios, no solo para empresas,
+            sino también para independientes.</v-card-text>
+          <v-img height="200px" src="https://images.pexels.com/photos/745243/pexels-photo-745243.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"> </v-img>
+          <v-card-subtitle>Español</v-card-subtitle>
+          <v-card-text>El español es la tercera lengua más hablada del mundo por el número de hablantes que la tienen
+            como lengua materna (tras el chino mandarín y el inglés). Lo hablan como primera y segunda lengua entre 450
+            y 500 millones de personas. Es el tercer idioma más hablado contando a los que lo hablan como primera o
+            segunda lengua, por detrás del chino mandarín y del inglés.</v-card-text>
+          <v-img height="200px" src="https://images.pexels.com/photos/54097/spain-flag-flutter-spanish-54097.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"> </v-img>
+        </v-card>
+        <v-spacer></v-spacer>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "more-spoken"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,8 @@
 
     <navbar></navbar>
     <banner></banner>
+    <more-spoken></more-spoken>
+    <testimonials></testimonials>
     <call-to-login></call-to-login>
     <ranking-student></ranking-student>
     <Footer> </Footer>
@@ -19,12 +21,15 @@ import Banner from "@/components/banner";
 import CallToLogin from "@/components/call-to-register";
 import Navbar from "@/components/Navbar";
 import RankingStudent from "@/components/RankingStudent";
+import Testimonials from "../components/testimonials";
+import MoreSpoken from "../components/more-spoken";
 export default {
   name: 'Home',
   components: {
+    MoreSpoken,
+    Testimonials,
     RankingStudent,
     Navbar,
-
     CallToLogin,
     Banner,
     Footer
