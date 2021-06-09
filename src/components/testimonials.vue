98f9ceb37ab2e1f3745b61cafe0aa3751e29fc50
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2021-06-08 19:27:04 -0500

    feat: add cards to testimonials component TODO: add styles with vuetify

---
+++ src/components/testimonials.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="6" sm="12" md="6">
+        <v-card elevation="2" outlined shaped>
+          <v-img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                 max-height="300"
+                 max-width="270"></v-img>
+          <v-card-title class="text-h3">John Doe</v-card-title>
+          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit ipsam quidem temporibus vitae?</v-card-text>
+          <v-card-actions>
+            <v-btn>Read More!</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="6" sm="12" md="6">
+        <v-card elevation="2" outlined shaped>
+          <v-img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                 max-height="300"
+                 max-width="270"></v-img>
+          <v-card-title class="text-h3">John Doe</v-card-title>
+          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit ipsam quidem temporibus vitae?</v-card-text>
+          <v-card-actions>
+            <v-btn>Read More!</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="6" sm="12" md="6">
+        <v-card elevation="2" outlined shaped>
+          <v-img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                 max-height="300"
+                 max-width="270"></v-img>
+          <v-card-title class="text-h3">John Doe</v-card-title>
+          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit ipsam quidem temporibus vitae?</v-card-text>
+          <v-card-actions>
+            <v-btn>Read More!</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="6" sm="12" md="6">
+        <v-card elevation="2" outlined shaped>
+          <v-img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                 max-height="300"
+                 max-width="270"></v-img>
+          <v-card-title class="text-h3">John Doe</v-card-title>
+          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit ipsam quidem temporibus vitae?</v-card-text>
+          <v-card-actions>
+            <v-btn>Read More!</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "testimonials"
+}
+</script>
+
+<style scoped>
+
+</style>
