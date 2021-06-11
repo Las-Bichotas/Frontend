93ec15c9566e1c2400b5c67873a345c286f2237e
Sebastian Toulier <65046165+SebTou08@users.noreply.github.com>
2021-06-10 20:21:44 -0500

    refactor: add some space in conditional text

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -11,28 +11,9 @@
         tile
         class="indigo lighten-1 white--text text-center"
     >
-      <v-card-text>
-        <v-btn
-            v-for="icon in icons"
-            :key="icon"
-            class="mx-4 white--text"
-            icon
-        >
-          <v-icon size="24px">
-            {{ icon }}
-          </v-icon>
-        </v-btn>
-      </v-card-text>
 
-      <v-card-text class="white--text pt-0">
-        <strong>ILanguage</strong> es una aplicación web y móvil que permite aprender y/o perfeccionar diversos idiomas. Los usuarios son personas que quieran especializarse o practicar más algún idioma. Por ello, es un requisito que los usuarios tengan un conocimiento básico, por lo menos. La app contará con una sola versión, la cual es mediante suscripciones.
-      </v-card-text>
 
-      <v-divider></v-divider>
 
-      <v-card-text class="white--text">
-        {{ new Date().getFullYear() }} — <strong>ILanguage</strong>
-      </v-card-text>
       <v-card-text class="white--text">
         <v-row justify="center">
           <v-dialog
@@ -45,6 +26,7 @@
                   dark
                   v-bind="attrs"
                   v-on="on"
+                  class="btn-terminos"
               >
                 Terminos y condiciones
               </v-btn>
@@ -55,15 +37,15 @@
               </v-card-title>
               <v-card-text class="text-md-center text-justify">
                 Al acceder y utilizar este servicio, usted acepta y accede a estar obligado por los términos y disposiciones de este acuerdo. Asimismo, al utilizar estos servicios particulares, usted estará sujeto a toda regla o guía de uso correspondiente que se haya publicado para dichos servicios. Toda participación en este servicio constituirá la aceptación de este acuerdo. Si no acepta cumplir con lo anterior, por favor, no lo utilice.
-                <strong>Politica de privacidad \n </strong> Ilanguage te informa sobre su Política de Privacidad respecto del tratamiento y protección de los datos de carácter personal de los usuarios y clientes que puedan ser recabados por la navegación o contratación de servicios a través del sitio Web www.ilanguage.com
+                <strong class="strong-text"><br>Politica de privacidad <br></strong>  Ilanguage  te informa sobre su Política de Privacidad respecto del tratamiento y protección de los datos de carácter personal de los usuarios y clientes que puedan ser recabados por la navegación o contratación de servicios a través del sitio Web www.ilanguage.com
                 En este sentido, el Titular garantiza el cumplimiento de la normativa vigente en materia de protección de datos personales, reflejada en la Ley Orgánica 3/2018, de 5 de diciembre, de Protección de Datos Personales y de Garantía de Derechos Digitales (LOPD GDD). Cumple también con el Reglamento (UE) 2016/679 del Parlamento Europeo y del Consejo de 27 de abril de 2016 relativo a la protección de las personas físicas (RGPD).
-                El uso de sitio Web implica la aceptación de esta Política de Privacidad así como las condiciones incluidas en el Aviso Legal. <strong>Obtención de datos personales \n </strong> Para navegar por www.ilanguage.com no es necesario que facilites ningún dato personal. Los casos en los que sí proporcionas tus datos personales son los siguientes:
+                El uso de sitio Web implica la aceptación de esta Política de Privacidad así como las condiciones incluidas en el Aviso Legal. <strong> <br> Obtención de datos personales <br> </strong> Para navegar por www.ilanguage.com no es necesario que facilites ningún dato personal. Los casos en los que sí proporcionas tus datos personales son los siguientes:
                 <ul id="example-2">
                   <li v-for="(datoPersonal) in datosPersonales" v-bind:key="datoPersonal.id">
                      {{ datoPersonal.content }}
                   </li>
                 </ul>
-                <strong>Tus derechos</strong> El Titular te informa que sobre tus datos personales tienes derecho a: /n
+                <strong  ><br>Tus derechos <br> </strong> El Titular te informa que sobre tus datos personales tienes derecho a: /n
                 <ul id="example-1">
                   <li v-for="(derecho) in derechos" v-bind:key="derecho.id">
                     {{ derecho.content }}
@@ -74,9 +56,9 @@
                 Tienes derecho a la tutela judicial efectiva y a presentar una reclamación ante la autoridad de control, en este caso, la Agencia Española de Protección de Datos, si consideras que el tratamiento de datos personales que te conciernen infringe el Reglamento.
                 Las páginas de este sitio Web pueden incluir contenido incrustado (por ejemplo, vídeos, imágenes, artículos, etc.). El contenido incrustado de otras web se comporta exactamente de la misma manera que si hubieras visitado la otra web.
                 Estos sitios Web pueden recopilar datos sobre ti, utilizar cookies, incrustar un código de seguimiento adicional de terceros, y supervisar tu interacción usando este código.
-                <strong>Contenido de otros sitios web </strong> Las páginas de este sitio Web pueden incluir contenido incrustado (por ejemplo, vídeos, imágenes, artículos, etc.). El contenido incrustado de otras web se comporta exactamente de la misma manera que si hubieras visitado la otra web.
+                <strong> <br> Contenido de otros sitios web  <br> </strong> Las páginas de este sitio Web pueden incluir contenido incrustado (por ejemplo, vídeos, imágenes, artículos, etc.). El contenido incrustado de otras web se comporta exactamente de la misma manera que si hubieras visitado la otra web.
                 Estos sitios Web pueden recopilar datos sobre ti, utilizar cookies, incrustar un código de seguimiento adicional de terceros, y supervisar tu interacción usando este código.
-                <strong>Política de cookies</strong> Para que este sitio Web funcione correctamente necesita utilizar cookies, que es una información que se almacena en tu navegador web.
+                <strong> <br> Política de cookies <br> </strong> Para que este sitio Web funcione correctamente necesita utilizar cookies, que es una información que se almacena en tu navegador web.
                 En la página Política de Cookies puedes consultar toda la información relativa a la política de recogida, la finalidad y el tratamiento de las cookies.
                 Esta página y sus componentes se ofrecen únicamente con fines informativos; esta página no se hace responsable de la exactitud, utilidad o disponibilidad de cualquier información que se transmita o ponga a disposición a través de la misma; no será responsable por cualquier error u omisión en dicha información”.Este tipo de disposición te ayudará a evitar responsabilidad en caso de que un usuario se base en tu información y eso cause un problema.
                 La página y su contenido original, las características y la funcionalidad son propiedad de ILanguage S.A.C y están protegidos por derechos de autor internacionales, marcas registradas, patentes, secretos comerciales y demás leyes de propiedad intelectual o de derechos de propiedad.
@@ -104,7 +86,15 @@
           </v-dialog>
         </v-row>
       </v-card-text>
+      <v-card-text class="white--text pt-0">
+        <strong>ILanguage</strong> es una aplicación web y móvil que permite aprender y/o perfeccionar diversos idiomas. Los usuarios son personas que quieran especializarse o practicar más algún idioma. Por ello, es un requisito que los usuarios tengan un conocimiento básico, por lo menos. La app contará con una sola versión, la cual es mediante suscripciones.
+      </v-card-text>
 
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} — <strong>ILanguage</strong>
+      </v-card-text>
     </v-card>
   </v-footer>
 </template>
@@ -114,6 +104,7 @@ export default {
   name: "footer",
   data(){
     return {
+      dialog: false,
       datosPersonales: [
         {id:1, content:"Al contactar a través de los formularios de contacto o enviar un correo electrónico."},
         {id:2, content: "Al realizar un comentario en un artículo o página."},
@@ -136,6 +127,15 @@ export default {
 #footer{
   background-color: #42b983 !important;
   color: #42b983;
+}
+
+.btn-terminos{
+  margin-top: 10px;
+  border-bottom: 1px solid;
+  padding-bottom: 10px;
+}
+.strong-text{
+  text-decoration-skip-ink: revert;
 }
 
 </style>
