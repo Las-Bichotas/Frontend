10a813fa96b43785677f8970e1c0369173ddb4c9
David-lp-ops <54964742+David-lp-ops@users.noreply.github.com>
2021-06-11 23:05:27 -0500

    fix:FixNavbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,9 @@
-<template>
-  <v-card
-      class="mx-auto overflow-hidden"
 
-  >
+<template>
+  <div id = "app">
+
+
+
     <v-app-bar
         color="primary"
         dark
@@ -10,14 +11,14 @@
         scroll-target="#scrolling-techniques-7"
     >
 
-
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="hidden-md-and-up"></v-app-bar-nav-icon>
       <v-toolbar-title>ILanguage</v-toolbar-title>
       <v-spacer></v-spacer>
 
-      <div class="text-center">
-        <div>
+
+          <v-toolbar-items class="hidden-sm-and-down" >
           <v-btn
-              class="ma-2"
+
               color="red"
               dark
           >
@@ -31,9 +32,10 @@
           </v-btn>
 
           <v-btn
-              class="ma-2"
+
               color="success"
               dark
+
           >
             Log in
             <v-icon
@@ -43,9 +45,13 @@
               mdi-robot-happy
             </v-icon>
           </v-btn>
-        </div>
-      </div>
+          </v-toolbar-items>
+
+
     </v-app-bar>
+
+
+
     <v-sheet
         id="scrolling-techniques-7"
         class="overflow-y-auto"
@@ -58,6 +64,8 @@
         v-model="drawer"
         absolute
         temporary
+        width = "200"
+        id = "drawer"
     >
       <v-list
           nav
@@ -74,6 +82,8 @@
             <v-list-item-title>Home</v-list-item-title>
           </v-list-item>
 
+
+
           <v-list-item>
             <v-list-item-icon>
               <v-icon>mdi-account</v-icon>
@@ -87,10 +97,25 @@
             </v-list-item-icon>
             <v-list-item-title>Teacher</v-list-item-title>
           </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account-circle</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Login</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-cloud</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Register</v-list-item-title>
+          </v-list-item>
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
-  </v-card>
+
+
+</div>
 </template>
 
 <script>
