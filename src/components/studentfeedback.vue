c2136712786fb3598fa861afe4b712b0355179df
Sebastian Trujillo <ippo_sebas@hotmail.com>
2021-06-12 16:19:52 -0500

    Add: Finish the student feedback component

--- src/components/studentfeedback.vue
+++ src/components/studentfeedback.vue
@@ -1,79 +1,81 @@
 <template>
-      <div>
-        <h1 class="text-h2 text-lg-h1 text-md-h2 text-sm-h2 mb-5 text-center">FeedBack</h1>
-        <p class="text-lg-h7 text-center">
-          Un beneficio que tendran los profesores y los alumnos sera que podran ser
-          calificados por los demas usuarios con un sistema sencillo. El usuario
-          que es calificado podra ver las calificaciones que le dan.
-        </p>
-        <v-card elevation="24" max-width="500" class="mx-auto">
-          <v-img height="250px"  src="../assets/student1.png" ></v-img>
-          <H2 class="text-center">Juan carlos</H2>
-          <br>
-          <div>
-            <div class="fill-height" align="center" justify="center">
+  <div>
+    <h1 class="text-h2 text-lg-h1 text-md-h2 text-sm-h2 mb-5 text-center">FeedBack</h1>
+    <p class="text-lg-h7 text-center">
+      Un beneficio que tendran los profesores y los alumnos sera que podran ser
+      calificados por los demas usuarios con un sistema sencillo y el usuario
+      que es calificado podra ver las calificaciones que le dan en porcentajes.
+    </p>
+    <v-container>
+      <v-row>
+        <v-col cols="6">
+          <v-img height="150px" src="../assets/student1.png"></v-img>
+          <h2 class="text-center">Juan carlos</h2>
+        </v-col>
+        <v-col cols="6">
+          <v-row class="d-flex justify-center" >
+            <div >
+              <v-rating color="warning" half-increments hover length="5" size="38" value="3"></v-rating>
+            </div>
+          </v-row>
+          <v-row>
+            <v-col>
               1 Estrella
-              <v-progress-linear
+              <v-progress-circular
                   value="15"
                   color="purple lighten-3"
                   height="15"
-              ></v-progress-linear>
-            </div>
-
-            <br>
-            <div class="fill-height" align="center" justify="center">
+              ></v-progress-circular>
+            </v-col>
+            <v-col>
               2 Estrellas
-              <v-progress-linear
+              <v-progress-circular
                   value="21"
                   color="blue lighten-3"
                   height="15"
-              ></v-progress-linear>
-            </div>
-            <br>
-            <div class="fill-height" align="center" justify="center">
+              ></v-progress-circular>
+            </v-col>
+            <v-col>
               3 Estrellas
-              <v-progress-linear
+              <v-progress-circular
                   value="40"
                   color="grey lighten-1"
                   height="15"
-              ></v-progress-linear>
-            </div>
-            <br>
-            <div class="fill-height" align="center" justify="center">
+              ></v-progress-circular>
+            </v-col>
+            <v-col>
               4 Estrellas
-              <v-progress-linear
+              <v-progress-circular
                   value="55"
                   color="blue-grey darken-1"
                   height="15"
-              ></v-progress-linear>
-            </div>
-            <br>
-            <div class="fill-height" align="center" justify="center">
+              ></v-progress-circular>
+            </v-col>
+            <v-col>
               5 Estrellas
-              <v-progress-linear
+              <v-progress-circular
                   value="75"
                   color="amber"
                   height="15"
-              ></v-progress-linear>
-            </div>
-          </div>
-        </v-card>
-      </div>
+              ></v-progress-circular>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 
 </template>
-
-
 
 <script>
 export default {
   name: "studentfeedback",
   data: () => ({
-    skill: 20,
-    knowledge: 33,
-    power: 78,
+    rating: 4.5,
   }),
 }
 </script>
+
 
 <style scoped>
 
