91426892099f5d3abe289679a704488269acfd3a
sebaas14toulier@gmail.com <sebaas14toulier@gmail.com>
2021-06-13 20:45:24 -0500

    feat: add role tutor components

---
+++ src/components/tutor-student-component.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "tutor-student-component"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/tutor-student.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "tutor-student"
+}
+</script>
+
+<style scoped>
+
+</style>
