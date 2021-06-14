c4b4cf519af0eca911253665bffae2f2b4e685e1
Sebastian Trujillo <ippo_sebas@hotmail.com>
2021-06-14 15:00:16 -0500

    Add: Finish everything about badge and feedback component

--- src/components/badge-component.vue
+++ src/components/badge-component.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 class="text-h2 text-lg-h1 text-md-h2 text-sm-h2 mb-5 text-center">Insignias</h1>
     <p class="text-lg-h7 text-center">
-      Estas son algunas de las insignias que se puede coseguir en nuestra aplicacion. Estas diferentes insignias son para los estudiantes y profesores
+      Estas son algunas de las insignias que se puede conseguir en nuestra aplicación. Estas diferentes insignias son para los estudiantes y profesores con las cuales pueden mostrarlas a los demás usuarios que visitan sus perfiles.
     </p>
     <v-container>
       <v-row>
@@ -17,6 +17,7 @@
             </v-carousel>
           </v-col>
         </v-card>
+        <h2>Insignias Profesores</h2>
         <v-card :loading="loading" class="mx-auto my-12" max-width="500">
           <v-col>
             <v-carousel :show-arrows="false" :cycle="true">
@@ -28,6 +29,8 @@
             </v-carousel>
           </v-col>
         </v-card>
+        <v-spacer></v-spacer>
+
         <v-card :loading="loading" class="mx-auto my-12" max-width="500">
           <v-col>
             <v-carousel :show-arrows="false" :cycle="true">
@@ -39,6 +42,8 @@
             </v-carousel>
           </v-col>
         </v-card>
+
+        <h2>Insignias Alumnos</h2>
         <v-card :loading="loading" class="mx-auto my-12" max-width="500">
           <v-col>
             <v-carousel :show-arrows="false" :cycle="true">
@@ -61,26 +66,26 @@ export default {
   data() {
     return {
       items: [
-        {src: require("../assets/1.1.png")},
-        {src: require("../assets/2.1.png")},
-        {src: require("../assets/3.1.png")},
-        {src: require("../assets/4.1.png")},
+        {src: require("../assets/profesor1.1.png")},
+        {src: require("../assets/profesor2.2.png")},
+        {src: require("../assets/profesor3.1.png")},
+        {src: require("../assets/profesor4.1.png")},
       ],
       imgs: [
-        {src: require("../assets/1.2.png")},
-        {src: require("../assets/2.2.png")},
-        {src: require("../assets/3.2.png")},
-        {src: require("../assets/4.2.png")},],
+        {src: require("../assets/profesor1.3.png")},
+        {src: require("../assets/Profesor2.4.png")},
+        {src: require("../assets/profesor3.2.png")},
+        {src: require("../assets/profesor4.4.png")},],
       cards: [
-        {src: require("../assets/1.3.png")},
-        {src: require("../assets/2.3.png")},
-        {src: require("../assets/3.3.png")},
-        {src: require("../assets/4.3.png")},],
+        {src: require("../assets/alumno1.2.png")},
+        {src: require("../assets/alumno2.1.png")},
+        {src: require("../assets/alumno3.3.png")},
+        {src: require("../assets/alumno4.2.png")},],
       imgss: [
-        {src: require("../assets/1.4.png")},
-        {src: require("../assets/2.4.png")},
-        {src: require("../assets/3.4.png")},
-        {src: require("../assets/4.4.png")},],
+        {src: require("../assets/alumno1.4.png")},
+        {src: require("../assets/alumno2.3.png")},
+        {src: require("../assets/alumno3.4.png")},
+        {src: require("../assets/alumno4.3.png")},],
     }
   },
 }
