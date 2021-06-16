cb1e89aebbca0b8b75dacff5cfe68bfd02c4eae7
XantX <hp_xantx@hotmail.com>
2021-06-15 22:32:03 -0500

    fix: distribution of home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,21 @@
 <template>
   <div class="home">
-
     <banner></banner>
     <more-spoken></more-spoken>
-
+    <badge-component></badge-component>
+    <testimonials v-bind:user-visibility="true"></testimonials>
     <v-row>
       <v-col cols="4">
         <ranking-student></ranking-student>
       </v-col>
       <v-col cols="8">
-    <v-container fluid>
-      <studentfeedback></studentfeedback>
-    </v-container>
+        <v-container fluid>
+          <studentfeedback></studentfeedback>
+        </v-container>
       </v-col>
     </v-row>
-    <testimonials v-bind:user-visibility="true"></testimonials>
-    <badge-component></badge-component>
     <testimonials v-bind:tutor-visibility="true"></testimonials>
     <call-to-login></call-to-login>
-
     <Footer> </Footer>
   </div>
 </template>
@@ -32,12 +29,11 @@ import CallToLogin from "@/components/call-to-register";
 import RankingStudent from "@/components/ranking-student";
 import Testimonials from "../components/testimonials";
 import MoreSpoken from "../components/more-spoken";
-import Studentfeedback from '../components/studentfeedback.vue';
+import Studentfeedback from "../components/studentfeedback.vue";
 import BadgeComponent from "../components/badge-component";
 export default {
   name: "Home",
   components: {
-
     BadgeComponent,
     MoreSpoken,
     Testimonials,
