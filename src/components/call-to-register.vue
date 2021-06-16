bef188331b9ad383c85a2b9b48ef6251c7a5b917
XantX <hp_xantx@hotmail.com>
2021-06-16 01:05:57 -0500

    fix: fix call to subcrib seccion

--- src/components/call-to-register.vue
+++ src/components/call-to-register.vue
@@ -1,6 +1,6 @@
 <template>
   <v-banner elevation="4">
-    <v-container class="d-flex justify-center">
+    <v-container>
       <v-row>
         <v-col lg="6" class="d-flex justify-center order-2 order-lg-1">
           <div>
@@ -12,24 +12,36 @@
             ></v-img>
           </div>
         </v-col>
-        <v-col lg="6" class="d-flex align-center justify-center order-1 order-lg-2">
+        <v-col
+          lg="6"
+          class="d-flex align-center justify-center order-1 order-lg-2"
+        >
           <div>
             <h1 class="text-h3 text-lg-h1 text-md-h2 text-sm-h2 mb-6">
               Suscribete ya
             </h1>
-            <p class="text-h6">
-              Subscribe now and live the best language learning experience learning with native tutors. Have conversations with native speakers and practice your fluency in our app.
+            <p class="text-h6 text-justify">
+              Subscribe now and live the best language learning experience
+              learning with native tutors. Have conversations with native
+              speakers and practice your fluency in our app.
             </p>
             <v-divider class="mb-4"></v-divider>
 
-            <p class="text-h6">
-              Join our staff of tutors and share your knowledge in your own language.
+            <p class="text-h6 text-justify">
+              Join our staff of tutors and share your knowledge in your own
+              language.
             </p>
-            <router-link :to="{name: 'Login'}">
-            <v-btn block large color="primary" elevation="6" class="mb-5" >Login</v-btn></router-link>
-
-            <router-link :to="{name: 'Register'}">
-              <v-btn block large elevation="6" outlined> Register</v-btn></router-link>
+            <div>
+              <router-link :to="{ name: 'Login' }">
+                <v-btn block large color="primary" elevation="6" class="mb-5"
+                  >Login</v-btn
+                ></router-link>
+              <router-link :to="{ name: 'Register' }">
+                <v-btn block large elevation="6" outlined>
+                  Register</v-btn
+                ></router-link
+              >
+            </div>
           </div>
         </v-col>
       </v-row>
