4603e7c734a6f67bc80f97af3ec4a8a5b6af2160
sebaas14toulier@gmail.com <sebaas14toulier@gmail.com>
2021-06-22 14:40:44 -0500

    feat: add sessions by API

--- src/components/session.vue
+++ src/components/session.vue
@@ -1,105 +1,120 @@
 <template>
-  <v-layout row wrap align-center>
-    <v-flex xs8 offset-md2 offset-2>
-      <div v-for="session in sessions" :key="session.id">
-        <v-card elevation="2" class="mx-auto my-3" data-aos-easing="ease">
-          <v-card-title>{{ session.title }} {{session.day}}</v-card-title>
-          <v-card-text>
-            <v-chip-group column>
-              <v-chip>{{ session.init }}</v-chip>
-              <v-chip>{{ session.finish }}</v-chip>
-            </v-chip-group>
-          </v-card-text>
-          <v-card-text>
-            <v-row align="center" class="mx-0">
-              <div>{{ session.description }}</div>
-            </v-row>
-          </v-card-text>
-          <v-card-text>
-            <v-chip-group column>
-              <v-chip>{{ session.topic }}</v-chip>
-              <v-chip>{{ session.language }}</v-chip>
-            </v-chip-group>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn>CANCEL</v-btn>
+  <div>
+    <v-row>
 
-            <v-dialog v-model="dialog" width="700">
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn color="primary " dark v-bind="attrs" v-on="on">
-                  ENTER TO SESSION
-                </v-btn>
-              </template>
+    <v-container v-for="session in state.sessions"
+         :key="session.id"
+                 class="ctn-session"
+    >
 
-              <v-card>
-                <v-card-title class="text-h5 grey lighten-2">
-                  Session day {{ session.day }} start {{ session.init }}
-                </v-card-title>
+      <v-card
+          class="mx-auto  crd-sessions"
+          shaped
+          outlined
+          elevation="15"
+      >
+        <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            height="200px"
+        ></v-img>
 
-                <v-card-text>
-                  <v-card-title>
-                    {{ session.description }}
-                  </v-card-title>
-                  <v-card-title>
-                    Enter the session:
-                    {{ session.link }}
-                  </v-card-title>
-                </v-card-text>
+        <v-card-title>
+          {{session.topic}}
+        </v-card-title>
 
-                <v-divider></v-divider>
+        <v-card-subtitle>
+          1,000 miles of wonder
+        </v-card-subtitle>
+        <v-divider></v-divider>
 
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="primary" text @click="dialog = false">
-                    Copy link
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-          </v-card-actions>
-        </v-card>
-      </div>
-    </v-flex>
-  </v-layout>
+        <v-card-text>
+          I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for
+          sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey,
+          you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to
+          escape.
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn
+              color="orange lighten-2"
+              text
+              class="btn-reservar"
+          >
+            Explore
+          </v-btn>
+
+          <v-spacer></v-spacer>
+
+
+        </v-card-actions>
+
+
+      </v-card>
+
+
+    </v-container>
+
+    </v-row>
+  </div>
 </template>
 
 <script>
+import Vue from "vue";
+import VueCompositionAPI from "@vue/composition-api";
+
+Vue.use(VueCompositionAPI);
+import {reactive, onMounted} from "@vue/composition-api";
+
 export default {
   name: "session",
-  data() {
-    return {
-      dialog: false,
-      sessions: [
-        {
-          id: 1,
-          title: "Session",
-          day: "14/05/2021",
-          init: "7:00pm",
-          finish: "8:00pm",
-          description:
-            "In the conversation you have to bring topics on the topic to discuss.",
-          topic: "Math",
-          language: "English",
-          link: "https://zoom.us/j/1100000",
-        },
-        {
-          id: 2,
-          title: "Session",
-          day: "20/05/2021",
-          init: "6:00pm",
-          finish: "7:00pm",
-          description:
-            "In the conversation you have to bring topics on the topic to discuss.",
-          topic: "Vue js",
-          language: "English",
-          link: "https://zoom.us/j/1100000",
-        },
-      ],
-    };
-  },
 
-  props: {},
+  setup() {
+    const axios = require("axios");
+    const state = reactive({
+      sessions: []
+    })
+
+    onMounted(() => {
+      loadSesions();
+    })
+
+    function loadSesions() {
+      axios.get("https://60c2e09f917002001739da47.mockapi.io/session")
+          .then(function (response) {
+            for (let i = 0; i < response.data.length; i++) {
+              state.sessions.push(response.data[i]);
+
+            }
+            console.log(response.data);
+            console.log(state.sessions);
+          })
+          .catch((err) => console.log(err));
+    }
+
+
+    return {
+      state
+    }
+
+
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+@media (min-width: 960px) {
+  .crd-sessions{
+    margin-left: 10% !important;
+    margin-right: 10% !important;
+    margin-top: 10% !important;
+    max-width: 500px;
+  }
+}
+
+.btn-reservar{
+  flex: 10 !important;
+}
+.ctn-session{
+  max-width:500px !important;
+}
+</style>
