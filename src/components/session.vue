1f151edf1f55c87d46e26ca48ec22c7370f30554
sebaas14toulier@gmail.com <sebaas14toulier@gmail.com>
2021-06-22 17:09:23 -0500

    feat: add sessions by API

--- src/components/session.vue
+++ src/components/session.vue
@@ -14,7 +14,7 @@
           elevation="15"
       >
         <v-img
-            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            :src="state.imgs[parseInt(Math.random()*5)]"
             height="200px"
         ></v-img>
 
@@ -23,15 +23,14 @@
         </v-card-title>
 
         <v-card-subtitle>
-          1,000 miles of wonder
+          {{ session.startAt }} - {{session.endsAt}}
         </v-card-subtitle>
         <v-divider></v-divider>
 
         <v-card-text>
-          I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for
-          sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey,
-          you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to
-          escape.
+          {{
+            session.information
+          }}
         </v-card-text>
 
         <v-card-actions>
@@ -39,13 +38,17 @@
               color="orange lighten-2"
               text
               class="btn-reservar"
+
           >
-            Explore
+            Reservar
           </v-btn>
 
           <v-spacer></v-spacer>
 
 
+          <div class="card-footer pb-0 pt-3">
+            <jw-pagination :items="session" @changePage="onChangePage"></jw-pagination>
+          </div>
         </v-card-actions>
 
 
@@ -59,11 +62,19 @@
 </template>
 
 <script>
-import Vue from "vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+
 import VueCompositionAPI from "@vue/composition-api";
 
 Vue.use(VueCompositionAPI);
 import {reactive, onMounted} from "@vue/composition-api";
+
+import JwPagination from 'jw-vue-pagination';
+Vue.component('jw-pagination', JwPagination);
 
 export default {
   name: "session",
@@ -71,7 +82,9 @@ export default {
   setup() {
     const axios = require("axios");
     const state = reactive({
-      sessions: []
+      sessions: [],
+      pageOfItems: [],
+      imgs: ["https://m5s7j2f7.rocketcdn.me/wp-content/uploads/fly-images/8825/5-angular-vs_angularjs-vs-angular4-2136x0.jpg","https://conceptodefinicion.de/wp-content/uploads/2019/08/Medicina-.jpg","https://static-wc.arcux.net/uploads/20200529130551/la-arquitectura-1.jpg","https://www.caracteristicas.co/wp-content/uploads/2018/10/matematicas-e1581735212109.jpg","https://sites.google.com/site/divierteteconlafisica/_/rsrc/1427808642745/home/fisica_0.jpg" ],
     })
 
     onMounted(() => {
@@ -85,15 +98,22 @@ export default {
               state.sessions.push(response.data[i]);
 
             }
+            console.log(response);
             console.log(response.data);
             console.log(state.sessions);
           })
           .catch((err) => console.log(err));
     }
 
+    function onChangePage(pageOfItems){
+      state.pageOfItems = pageOfItems;
+    }
+
+
 
     return {
-      state
+      state,
+      onChangePage
     }
 
 
