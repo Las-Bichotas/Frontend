99a44f228f686453e4ce6fbd0807b3a61cea4549
sebaas14toulier@gmail.com <sebaas14toulier@gmail.com>
2021-06-22 17:30:40 -0500

    feat: add payment dialog

--- src/components/subscription-item.vue
+++ src/components/subscription-item.vue
@@ -67,19 +67,48 @@
           </v-card-text>
 
           <v-card-actions>
-            <router-link
-              style="text-decoration: none; color: inherit;"
-              :to="{ name: 'main' }"
-            >
-              <v-btn
-                color="primary lighten-2"
-                text
-                @click="reserve"
-                class="btnSeleccionar"
-              >
-                Select
-              </v-btn></router-link
-            >
+
+              <div class="text-center">
+                <v-dialog
+                    v-model="dialog"
+                    width="500"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                        color="orange lighten-2"
+                        dark
+                        v-bind="attrs"
+                        v-on="on"
+                    >
+                      Subscribe
+                    </v-btn>
+                  </template>
+
+                  <v-card>
+                    <v-card-title class="text-h5 grey lighten-2">
+                      Payment
+                    </v-card-title>
+
+                    <v-card-text>
+
+                    </v-card-text>
+
+                    <v-divider></v-divider>
+
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn
+                          color="primary"
+                          text
+                          @click="dialog = false"
+                      >
+                        I accept
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </div>
+
           </v-card-actions>
         </v-card>
       </div>
