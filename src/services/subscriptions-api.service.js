9e34c793fec82156e86b1f165e2c814015a69807
sebaas14toulier@gmail.com <sebaas14toulier@gmail.com>
2021-06-23 18:58:13 -0500

    feat: add axios method to crud subscriptions

--- src/services/subscriptions-api.service.js
+++ src/services/subscriptions-api.service.js
@@ -5,6 +5,30 @@ class SubscriptionsApiService {
         //return http.get('/api/subscriptions'); 
         return http.get('/subscription'); //mockito
     }
+
+    create(data) {
+        return http.post('/api/subscriptions', data);
+    }
+
+    get(id) {
+        return http.get(`/api/subscriptions/${id}`);
+    }
+
+    update(id, data) {
+        return http.put(`/api/subscriptions/${id}`, data);
+    }
+
+    getByName(name) {
+        return http.get(`/api/subscriptions/${name}`);
+    }
+
+    getByDuration(duration) {
+        return http.get(`/api/subscriptions/${duration}`);
+    }
+
+    delete(id) {
+        return http.delete(`/api/subscriptions/${id}`);
+    }
 }
 
 export default new SubscriptionsApiService();
