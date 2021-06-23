e68172144953b9b132ec5d249ed68e6087f9e8ee
sebaas14toulier@gmail.com <sebaas14toulier@gmail.com>
2021-06-23 08:51:37 -0500

    refactor: resize pay buttons

--- src/views/payment.vue
+++ src/views/payment.vue
@@ -1,140 +1,161 @@
 <template>
   <v-app>
-
     <v-card
-        elevation="24"
-        outlined
-        shaped
-        class="justify-center mx-auto my-12 crdpay"
+      elevation="24"
+      outlined
+      shaped
+      class="justify-center mx-auto my-12 crdpay"
     >
-
-
-    <vue-c-card
+      <vue-c-card
         :number="state.cc.number"
         :holder="state.cc.holder"
         :exp="state.cc.exp"
         :cvc="state.cc.cvc"
         :is-typing-cvc="isTypingCvc"
-    class="cccrd"/>
+        class="cccrd"
+      />
 
       <v-row>
-        <v-col
-            cols="12"
-            md="6"
-        >
-          <v-text-field label="Credit Card Number" type="text" v-model="state.cc.number" id="number" class="txtcard"></v-text-field></v-col>
-
-        <v-col
-            cols="12"
-            md="6"
-        >
-          <v-text-field label="Credit Card Owner" type="text" v-model="state.cc.holder" id="holder" class="txtcard"></v-text-field></v-col>
-        <v-col
-            cols="12"
-            md="6"
-        >
-          <v-text-field label="Expiration date" type="text" v-model="state.cc.exp" id="exp" class="txtcard"></v-text-field></v-col>
-
-        <v-col
-            cols="12"
-            md="6"
-        >
-    <v-text-field class="txtcard" label="CVC" type="text" v-model="state.cc.cvc"
-           @focus="isTypingCvc = true"
-                  @blur="isTypingCvc = false" id="cvc"></v-text-field></v-col>
-
-        <v-col
-            cols="12"
-            md="6"
+        <v-col cols="12" md="6">
+          <v-text-field
+            label="Credit Card Number"
+            type="text"
+            v-model="state.cc.number"
+            id="number"
+            class="txtcard"
+          ></v-text-field
         ></v-col>
 
-      </v-row>
-      <v-btn
-          block
-          elevation="14"
-          rounded
-          text
-          class="btnPagar"
-      >Pagar</v-btn>
+        <v-col cols="12" md="6">
+          <v-text-field
+            label="Credit Card Owner"
+            type="text"
+            v-model="state.cc.holder"
+            id="holder"
+            class="txtcard"
+          ></v-text-field
+        ></v-col>
+        <v-col cols="12" md="6">
+          <v-text-field
+            label="Expiration date"
+            type="text"
+            v-model="state.cc.exp"
+            id="exp"
+            class="txtcard"
+          ></v-text-field
+        ></v-col>
 
-      <router-link
+        <v-col cols="12" md="6">
+          <v-text-field
+            class="txtcard"
+            label="CVC"
+            type="text"
+            v-model="state.cc.cvc"
+            @focus="isTypingCvc = true"
+            @blur="isTypingCvc = false"
+            id="cvc"
+          ></v-text-field
+        ></v-col>
+
+        <v-col cols="12" md="6"></v-col>
+      </v-row>
+      <v-row>
+<v-col cols="12" md="6">
+         <router-link
           style="text-decoration: none; color: inherit;"
           :to="{ name: 'home' }"
-
-      >
-        <br>
-      <v-btn
-          block
-          elevation="14"
+        >
+        <v-btn
+          color="primary"
+          elevation="24"
+          outlined
+          raised
           rounded
-          text class="btnCancelar"
-      >Cancelar</v-btn></router-link>
-
+          class="btnPagar"
+          >Pagar</v-btn
+        >
+         </router-link>
+</v-col>
+<v-col cols="12" md="6">
+        <router-link
+          style="text-decoration: none; color: inherit;"
+          :to="{ name: 'home' }"
+        >
+          <br />
+          <v-btn elevation="24" outlined raised rounded text class="btnCancelar"
+            >Cancelar</v-btn
+          ></router-link
+        >
+</v-col>
+      </v-row>
     </v-card>
   </v-app>
 </template>
 
 <script>
-import VueCCard from 'vue-ccard';
+import VueCCard from "vue-ccard";
 import Vue from "vue";
 import VueCompositionAPI from "@vue/composition-api";
 
 Vue.use(VueCompositionAPI);
 import { reactive } from "@vue/composition-api";
 
-
 export default {
   name: "payment",
-  components:{VueCCard},
-  setup(){
+  components: { VueCCard },
+  setup() {
     const state = reactive({
       valid: false,
       isTypingCvc: false,
       cc: {
-        number: '',
-        holder: '',
-        exp: '',
-        cvc: '',
+        number: "",
+        holder: "",
+        exp: "",
+        cvc: "",
       },
-    })
-
+    });
 
     return {
-      state
-    }
-  }
-}
+      state,
+    };
+  },
+};
 </script>
 
 <style scoped>
-.crdpay{
-margin-top: 10% !important;
+.crdpay {
+  margin-top: 10% !important;
 }
 
-.txtcard{
-  max-width:60%;
+.txtcard {
+  max-width: 60%;
 
   margin-left: 20% !important;
   margin-right: 20% !important;
 }
 
-.cccrd{
+.cccrd {
   margin-left: 20% !important;
   margin-right: 20% !important;
   margin-top: 5%;
   padding-bottom: 15px !important;
 }
-.btnCancelar{
+.btnCancelar {
   background-color: red !important;
-  margin-bottom: 5px !important;
-
+  
+  margin-left: 20% !important;
+ 
+  margin-right: 20% !important;
+  margin-bottom: 5% !important;
+  
 }
 
-.btnPagar{
+.btnPagar {
   background-color: #42b983;
+  margin-left:20% !important;
+  margin-right: 20% !important;
+  margin-top: 5% !important;
+ 
 
-  max-width: 80% !important;
 }
-
-
 </style>
