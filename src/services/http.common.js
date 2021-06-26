3e6424aa771a36751a20822a75e1915368814987
XantX <hp_xantx@hotmail.com>
2021-06-26 07:09:58 -0500

    fix: add proxy for use api in devserver TODO: test if the api don't make error in deploy server

--- src/services/http.common.js
+++ src/services/http.common.js
@@ -1,8 +1,9 @@
-import axios from 'axios'
-
+import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://ilenguageapi.azurewebsites.net',
-    //baseURL: 'https://60c2e09f917002001739da47.mockapi.io', //mockito
-    headers: { 'Content-type': 'application/json' }
+  baseURL: "http://localhost:8080",
+  //baseURL: 'https://ilenguageapi.azurewebsites.net',
+  //baseURL: 'https://60c2e09f917002001739da47.mockapi.io', //mockito
+  headers: { "Content-type": "application/json" },
 });
+
